29f6050a50690b8682f5f1b795b5fcb9e142e4fb
bengali-creates <debasish2005pal@gmail.com>
2025-08-11 23:57:25 +0530

    improved the backend

--- app/api/links/route.js
+++ app/api/links/route.js
@@ -1,7 +1,16 @@
 import connectDb from "@/db/connetdb";
 export async function POST(request) {
-    const body=await request.json();
-    console.log(body);
+    // const body=await request.json();
+    // console.log(body);
     await connectDb();
+    try {
+      const { links } = req.body;
+      const newDoc = new LinksArray({ links });
+      await newDoc.save();
+      res.status(201).json({ success: true, data: newDoc });
+    } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+    }
+    console.log('Request received:', body);
   return Response.json({ message: 'Hello World' })
 }
